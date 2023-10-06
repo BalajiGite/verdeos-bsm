43f8ba43acd0e54648e18f6b69a93cf0fcc107b6
Balaji Gite <balajigite16@gmail.com>
2023-10-06 11:51:35 +1100

    some code changes

--- amplify/backend/function/lmbdaDemoTest/src/index.py
+++ amplify/backend/function/lmbdaDemoTest/src/index.py
@@ -3,7 +3,7 @@ import json
 def handler(event, context):
   print('received event:')
   print(event)
-  
+  siteConnected = '{"totalSitePer":"67%", "totalReadings":"103/154", "sites":[{"id": "xyz", "name":"electrical", "color":"red", readings:"78/121"},{"id": "xyz", "name":"electrical", "color":"red", readings:"78/121"}]}'
   return {
       'statusCode': 200,
       'headers': {
@@ -11,5 +11,5 @@ def handler(event, context):
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
       },
-      'body': json.dumps('Hello from your new Amplify Python Balaji!')
+      'body': json.dumps(siteConnected)
   }
