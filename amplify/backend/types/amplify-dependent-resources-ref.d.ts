ac47bb53c06c808d04e4c7a8c17fd53929c82510
Balaji Gite <balajigite16@gmail.com>
2023-10-11 11:51:10 +1100

    some backend code push

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,17 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-    "function": {
-        "lmbdaDemoTest": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        }
-    },
-    "api": {
-        "apiDemoTest": {
-            "RootUrl": "string",
-            "ApiName": "string",
-            "ApiId": "string"
-        }
-    }
-}
+export type AmplifyDependentResourcesAttributes = {}
