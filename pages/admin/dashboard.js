51d1a79e3baa70325e9f9d29fc87a0ad02c42d7f
Balaji Gite <balajigite16@gmail.com>
2023-10-21 21:51:51 +1100

    Added Portfolio Certification table

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -4,7 +4,7 @@ import React from "react";
 
 import CardLineChart from "components/Cards/CardLineChart.js";
 import CardBarChart from "components/Cards/CardBarChart.js";
-import CardPageVisits from "components/Cards/CardPageVisits.js";
+import PortfolioCertification from "widgets/PortfolioCertification.js";
 import CardSocialTraffic from "components/Cards/CardSocialTraffic.js";
 import PortfolioPerformanceChart from "components/PortfolioPerformanceChart.js"
 
@@ -25,7 +25,7 @@ export default function Dashboard() {
       
       <div className="flex flex-wrap mt-4">
         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-          <CardPageVisits />
+          <PortfolioCertification />
         </div>
         <div className="w-full xl:w-4/12 px-4">
           <CardSocialTraffic />
