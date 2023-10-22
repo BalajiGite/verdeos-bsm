b3401be6220dea0c5791e4e4ff09b11322ed9cc7
Balaji Gite <balajigite16@gmail.com>
2023-10-22 22:44:50 +1100

    some code changes

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -15,6 +15,12 @@ export default function Dashboard() {
     <>
       <div className="flex flex-wrap">
         <div className="w-full xl:w-12/12 mb-12 xl:mb-0 px-4">
+          <MapExample />
+        </div>
+      </div>
+
+      <div className="flex flex-wrap mt-4">
+        <div className="w-full xl:w-12/12 mb-12 xl:mb-0 px-4">
           <CardLineChart />
         </div>
       </div>
