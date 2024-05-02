452bbb0d80b6a310e7b215e37fc79a363ad99799
Achut Gite <achutgite3821@gmail.com>
2024-05-02 11:24:11 +0530

    notification-customization-02

--- layouts/Admin.js
+++ layouts/Admin.js
@@ -32,74 +32,83 @@ export default function Admin({ children }) {
           </div>
           {visiblenotification && (
             <div className="mx-14 mt-5 background-color-linear p-4 border rounded border-[#8E8E8E] relative">
-              <div className="absolute top-0 right-0"><img src="/img/close.png" className="h-6 w-6 cursor-pointer" onClick={()=>setVisibleNotification(!visiblenotification)}/></div>
-            <div className="flex justify-between">
-            <div className="border-r pr-5 border-[#8E8E8E] px-3">
-              <p className="text-xl font-semibold text-[#C5C5C5] mb-0 text-sm">Actionable Insights Summary</p>
-              <p className="text-[#C5C5C5] mb-0 text-sm">Since your last login you have:</p>
-              <p className="text-[#C5C5C5] mb-0 text-sm">11     Services calls initiated</p>
-              <p className="text-[#C5C5C5] mb-0 text-sm">14    New sites connected</p>
-              <p className="text-[#C5C5C5] mb-0 text-sm">154  Persistent alarms cleared</p>
-            </div>
-            <div className="mx-3 bg-[#0A1016] py-1 border border-[#8E8E8E] rounded">
-              <p className="text-[#C5C5C5] py-1 px-3 mb-0">2022 Environmental Sustainability Highlights</p>
-
-              <hr className=" border-[#8E8E8E] w-full" />
-              <div className="flex px-4 mt-1">
-                <div className="flex items-center px-5 border-r border-[#8E8E8E]">
-                  <img src="/img/Co2.png" className="h-10 w-10 object-contain" alt="Co2 group" />
-                  <p className="text-[#C5C5C5] px-1 mb-0"> Carbon Emissions <br /><span className="text-[#C5C5C5]">-86%</span></p>
+              <div className="absolute top-0 right-0"><img src="/img/close.png" className="h-6 w-6 cursor-pointer" onClick={() => setVisibleNotification(!visiblenotification)} /></div>
+              <div className=" justify-between grid grid-cols-2 gap-x-1 mt-1">
+                <div className="border-r  border-[#8E8E8E] px-3">
+                  <p className="text-xl font-semibold text-[#C5C5C5] mb-0 ">Actionable Insights Summary</p>
+                  <p className="text-[#C5C5C5] mb-0 text-sm">Since your last login you have:</p>
+                  <p className="text-[#C5C5C5] mb-0 text-sm">11     Services calls initiated</p>
+                  <p className="text-[#C5C5C5] mb-0 text-sm">14    New sites connected</p>
+                  <p className="text-[#C5C5C5] mb-0 text-sm">154  Persistent alarms cleared</p>
                 </div>
+                <div className=" bg-[#0A1016] py-1 border border-[#8E8E8E] rounded">
+                  <p className="text-[#C5C5C5] py-1 px-3 mb-0">2022 Environmental Sustainability Highlights</p>
 
-                <div className="flex items-center  border-r border-[#8E8E8E] px-5">
-                  <img src="/img/Vector.png" className="h-10 w-10 object-contain" alt="Vector" />
-                  <p className="text-[#C5C5C5] px-1 mb-0"> Energy<br /><span className="text-[#C5C5C5]">-53%</span></p>
+                  <hr className=" border-[#8E8E8E] w-full" />
+                  <div className="flex px-4 mt-1 justify-between ">
+                    <div className="flex items-center px-2 border-r border-[#8E8E8E]">
+                      <img src="/img/Co2.png" className="h-10 w-10 object-contain" alt="Co2 group" />
+                      <p className="text-[#C5C5C5] px-1 mb-0"> Carbon Emissions <br /><span className="text-[#C5C5C5]">-86%</span></p>
+                    </div>
+
+                    <div className="flex items-center  border-r border-[#8E8E8E] px-2">
+                      <img src="/img/Vector.png" className="h-10 w-10 object-contain" alt="Vector" />
+                      <p className="text-[#C5C5C5] px-1 mb-0"> Energy<br /><span className="text-[#C5C5C5]">-53%</span></p>
+                    </div>
+
+                    <div className="flex items-center px-2">
+                      <img src="/img/Layer_1.png" className="h-10 w-10 object-contain" alt="Layer" />
+                      <p className="text-[#C5C5C5] px-1 mb-0"> Water<br /><span className="text-[#C5C5C5]">-60%</span></p>
+                    </div>
+                  </div>
                 </div>
+              </div>
 
-                <div className="flex items-center px-5 border-r border-[#8E8E8E]">
-                  <img src="/img/Layer_1.png" className="h-10 w-10 object-contain" alt="Layer" />
-                  <p className="text-[#C5C5C5] px-1 mb-0"> Water<br /><span className="text-[#C5C5C5]">-60%</span></p>
+              <div className="mt-1 grid grid-cols-2 gap-x-1 ">
+                <div className="border border-[#8E8E8E] rounded bg-[#0A1016]">
+                  <p className="text-[#C5C5C5] py-1 px-3 mb-0">2024 Building Performance Summary (YTD)</p>
+                  <hr className=" border-[#8E8E8E] w-full" />
+
+                  <div className="flex justify-between">
+                    <div className="flex items-center px-1 border-r py-1 border-[#8E8E8E]">
+                      <img src="/img/vector-1.png" className="h-10 w-10 object-contain" alt="Vector" />
+                      <p className="text-[#C5C5C5] px-1 mb-0"> Portfolio Availability <br /><span className="text-[#C5C5C5]">+16%</span></p>
+                    </div>
+
+                    <div className="flex items-center  border-r border-[#8E8E8E] px-1">
+                      <img src="/img/vector-2.png" className="h-10 w-10 object-contain" alt="Vector" />
+                      <p className="text-[#C5C5C5] px-1 mb-0"> Electrical Costs<br /><span className="text-[#C5C5C5]">-18%</span></p>
+                    </div>
+
+                    <div className="flex items-center px-1 ">
+                      <img src="/img/vector-2.png" className="h-10 w-10 object-contain" alt="Layer" />
+                      <p className="text-[#C5C5C5] px-1 mb-0">Water Costs<br /><span className="text-[#C5C5C5]">-29%</span></p>
+                    </div>
+                  </div>
                 </div>
+                <div className="border border-[#8E8E8E] rounded bg-[#0A1016] ">
+                  <p className="text-[#C5C5C5] py-1 px-3 mb-0">2024 Building Performance Summary (YTD)</p>
+                  <hr className=" border-[#8E8E8E] w-full" />
 
-                <div className="flex items-center px-5  border-[#8E8E8E]">
-                  <img src="/img/trash.png" className="h-10 w-10 object-contain" alt="trash" />
-                  <p className="text-[#C5C5C5] px-1 mb-0"> WDR <br /><span className="text-[#C5C5C5]">+9%</span></p>
+                  <div className="flex justify-between">
+                    <div className="flex items-center px-1 border-r py-1 border-[#8E8E8E]">
+                      <img src="/img/vector-3.png" className="h-10 w-10 object-contain" alt="trash" />
+                      <p className="text-[#C5C5C5] px-1 mb-0"> Data Quality <br /><span className="text-[#C5C5C5]">+68%</span></p>
+                    </div>
+
+                    <div className="flex items-center  border-r border-[#8E8E8E] px-1">
+                      <img src="/img/vector-4.png" className="h-10 w-10 object-contain" alt="trash" />
+                      <p className="text-[#C5C5C5] px-1 mb-0"> Service Calls<br /><span className="text-[#C5C5C5]">-14%</span></p>
+                    </div>
+
+                    <div className="flex items-center px-1">
+                      <img src="/img/trash.png" className="h-10 w-10 object-contain" alt="trash" />
+                      <p className="text-[#C5C5C5] px-1 mb-0"> WDR <br /><span className="text-[#C5C5C5]">+9%</span></p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="mx-3 bg-[#0A1016] py-1 border border-[#8E8E8E] rounded mt-5">
-              <p className="text-[#C5C5C5] py-1 px-3 mb-0">2024 Building Performance Summary (YTD)</p>
-
-              <hr className=" border-[#8E8E8E] w-full" />
-              <div className="flex px-4 mt-1">
-                <div className="flex items-center px-5 border-r py-1 border-[#8E8E8E]">
-                  <img src="/img/vector-1.png" className="h-10 w-10 object-contain" alt="Vector" />
-                  <p className="text-[#C5C5C5] px-1 mb-0"> Portfolio Availability <br /><span className="text-[#C5C5C5]">+16%</span></p>
-                </div>
-
-                <div className="flex items-center  border-r border-[#8E8E8E] px-5">
-                  <img src="/img/vector-2.png" className="h-10 w-10 object-contain" alt="Vector" />
-                  <p className="text-[#C5C5C5] px-1 mb-0"> Electrical Costs<br /><span className="text-[#C5C5C5]">-18%</span></p>
-                </div>
-
-                <div className="flex items-center px-5 border-r border-[#8E8E8E]">
-                  <img src="/img/vector-2.png" className="h-10 w-10 object-contain" alt="Layer" />
-                  <p className="text-[#C5C5C5] px-1 mb-0">Water Costs<br /><span className="text-[#C5C5C5]">-29%</span></p>
-                </div>
-
-                <div className="flex items-center px-5 border-r border-[#8E8E8E]">
-                  <img src="/img/vector-3.png" className="h-10 w-10 object-contain" alt="trash" />
-                  <p className="text-[#C5C5C5] px-1 mb-0"> Data Quality <br /><span className="text-[#C5C5C5]">+68%</span></p>
-                </div>
-
-                <div className="flex items-center px-5  border-[#8E8E8E]">
-                  <img src="/img/vector-4.png" className="h-10 w-10 object-contain" alt="trash" />
-                  <p className="text-[#C5C5C5] px-1 mb-0"> Service Calls<br /><span className="text-[#C5C5C5]">-14%</span></p>
-                </div>
-              </div>
-            </div>
-          </div>
           )}
           {/* <AdminNavbar /> */}
           {/* Header */}
